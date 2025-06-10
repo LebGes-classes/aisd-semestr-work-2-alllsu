b9359782b0ec1e166a716ca40e2c2bec22a0579b
Alsu Gayfullina <allsugayfullina06@gmail.com>
2025-06-10 14:20:04 +0300

    Гайфуллина Алсу

---
+++ Combsort.cpp
@@ -0,0 +1,55 @@
+﻿#include <bits/stdc++.h>
+#include <cstdio>   // Для printf
+#include <utility>
+
+using namespace std;
+
+int getNextGap(int gap)
+{
+    gap = (gap * 10) / 13;
+
+    if (gap < 1)
+        return 1;
+    return gap;
+}
+
+void combSort(int a[], int n)
+{
+    // Инициализация зазора
+    int gap = n;
+
+    // Инициализация swapped как true, чтобы убедиться, что
+    // цикл выполняется
+    bool swapped = true;
+
+    while (gap != 1 || swapped == true) {
+        // Найти следующий зазор
+        gap = getNextGap(gap);
+
+        // Инициализация swapped как false, чтобы мы могли
+        // проверить, произошла замена или нет
+        swapped = false;
+
+        // Сравнить все элементы с текущим пробелом
+        for (int i = 0; i < n - gap; i++) {
+            if (a[i] > a[i + gap]) {
+                swap(a[i], a[i + gap]);
+                swapped = true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int a[] = { 8, 4, 1, 56, 3, -44, 23, -6, 28, 0 };
+    int n = sizeof(a) / sizeof(a[0]);
+
+    combSort(a, n);
+
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
---
+++ StoogeSort.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+void stoogeSort(int arr[], int l, int r)
+{
+    //Если массив пустой или содержит только 1 элемент
+    if (l >= r)
+        return;
+
+   //Если первый элемент больше последнего, то меняем их местами
+    if (arr[l] > arr[r])
+        swap(arr[l], arr[r]);
+
+    //Если в массиве больше 2 элементов
+    if (r - l + 1 > 2) {
+        int t = (r - l + 1) / 3;
+         
+        //Рекурсивно сортируем первые 2/3 элементов
+        stoogeSort(arr, l, r - t);
+
+        //Рекурсивно сортируем последние 2/3 элементов
+        stoogeSort(arr, l + t, r);
+
+        //Снова рекурсивно сортируем первые 2/3 элементов
+        stoogeSort(arr, l, r - t);
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 3, 5, 9, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    stoogeSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
